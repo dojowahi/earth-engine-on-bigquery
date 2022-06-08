48548d9761a37ac5b33ed9c2c3ab80305f4206e0
dojowahi <33106883+dojowahi@users.noreply.github.com>
2022-06-07 19:55:47 -0500

    Create setup.sh
    Execute this script after cloning the repo

---
+++ setup.sh
@@ -0,0 +1,40 @@
+# Cloud function setup for EE
+
+project_id=$(gcloud config get-value project)
+cf_name="farmNDVIcf"
+ee_sa="earth-engine-343601@appspot.gserviceaccount.com"
+
+cd ~/earth-engine-on-bigquery/cloud-functions/ndvi
+
+#Create the external connection for BQ
+
+bq mk --connection --display_name='my_gcf_ee_conn' \
+      --connection_type=CLOUD_RESOURCE \
+      --project_id=$(gcloud config get-value project) \
+      --location=US  gcf-ee-conn
+
+#Get serviceAccountID assocaited with the connection  
+
+serviceAccountId=`bq show --location=US --connection --format=json gcf-ee-conn| jq -r '.cloudResource.serviceAccountId'
+
+
+gcloud functions deploy ${cf_name} --entry-point get_ndvi_month --runtime python39 --trigger-http --allow-unauthenticated --set-env-vars SERVICE_ACCOUNT=${ee_sa} --project ${project_id} --service-account ${ee_sa} --memory 2048MB
+
+#Add Cloud Invoker function role
+
+gcloud projects add-iam-policy-binding \
+$(gcloud config get-value project) \
+--member='serviceAccount:${serviceAccountId} \
+--role='roles/cloudfunctions.invoker'
+
+
+endpoint=$(gcloud functions describe farmNDVI --region=us-central1 --format=json | jq -r '.httpsTrigger.url')
+
+bq show gee || bq mk -d gee
+
+    
+build_sql="CREATE OR REPLACE FUNCTION gee.get_ndvi_month(lon float64,lat float64, farm_name STRING, year int64, month int64) RETURNS STRING REMOTE WITH CONNECTION \`${project_id}.us.gcf-ee-conn\` OPTIONS ( endpoint = '${endpoint}')"
+
+    
+bq query --use_legacy_sql=false ${build_sql}
+
