dc2727275db6c26884f135efd48bb16a2b3d8081
dojowahi <33106883+dojowahi@users.noreply.github.com>
2022-06-15 14:28:22 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -46,5 +46,5 @@ bq query --use_legacy_sql=false ${build_sql}
 
 bq load --source_format=CSV --replace=true --skip_leading_rows=1  --schema=farm_aoi:STRING,name:STRING ${project_id}:gee.land_coords  ./farm_dim.csv 
 
-bq query --use_legacy_sql=false 'SELECT gee.get_poly_ndvi_month(farm_aoi,name,2020,7) as ndvi_jul FROM `earth-engine-343601.gee.land_coords` LIMIT 10'
+bq query --use_legacy_sql=false 'SELECT gee.get_poly_ndvi_month(farm_aoi,name,2020,7) as ndvi_jul FROM `earth-engine-343601.gee.farm_dim` LIMIT 10'
 
