53f5ed0cafa89fbaad42340e2a6edfd4e7abf66a
dojowahi <33106883+dojowahi@users.noreply.github.com>
2022-08-25 13:29:05 -0500

    Create config.sh

---
+++ config.sh
@@ -0,0 +1,13 @@
+# project id for your project
+export PROJECT_ID=<insert project id>
+
+# Service Account which has access to GEE and a private key created, check prerequisites
+export SERVICE_ACCOUNT=<Service account ID>
+
+
+##################################################
+#Example
+##################################################
+# export PROJECT_ID=gee-demo-test
+# export SERVICE_ACCOUNT=ee-tester@gee-demo-test.iam.gserviceaccount.com
+##################################################
