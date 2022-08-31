c0e35f8c4351e86ea60d7214fab816c3b956efef
dojowahi <33106883+dojowahi@users.noreply.github.com>
2022-08-31 17:25:09 -0500

    Update config.sh
    Added REGION back

--- config.sh
+++ config.sh
@@ -15,6 +15,9 @@ export PROJECT_ID=<insert project id>
 # the new project will need to be tied to a billing account, uncomment the line below for Argolis users and update value
 # export BILLING_ACCOUNT_ID=<insert billing account>
 
+# desired GCP region for networking and compute resources
+export REGION=<insert gcp region>
+
 # desired App Engine region name
 # may be slightly different from GCP region above
 # run "gcloud app regions list" from an existing project to confirm
@@ -26,5 +29,6 @@ export APP_ENGINE_REGION=<insert desired app engine region name>
 # export USER_EMAIL=myuser@mydomain.com
 # export PROJECT_ID=gee-on-gcp
 # export BILLING_ACCOUNT_ID=123456-123456-123456
+# export REGION=us-central1
 # export APP_ENGINE_REGION=us-central
 ##################################################
